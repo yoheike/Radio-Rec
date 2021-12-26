6d7c64c7418c2cc77f6ef0d5740100bf740ad2dd
yoheike <89015879+yoheike@users.noreply.github.com>
2021-12-26 12:51:47 +0900

    Support image notifications of LINE.

--- rec-onsen.sh
+++ rec-onsen.sh
@@ -47,13 +47,14 @@ function onsen_download(){
 	ffmpeg -i ${streaming_url} ${codec_option} -acodec copy -bsf:a aac_adtstoasc "${filename}"
 	mv ./"${filename}" ./"${filename}.org"
 	ffmpeg -i "${filename}.org" -i "${target_title} ${title} ${delivery_date}放送${guest_title}.jpg" -map 0:a -map 1:v -disposition:1 attached_pic -metadata "title=${target_title} ${title}" -metadata "artist=${performers}" -metadata "comment=${guest_option}" -metadata "album=${target_title}" -metadata "track=${track_no}" -metadata "date=${delivery_date}" -c copy "${filename}"
-	rm ./*.org ./*.jpg
+	rm ./*.org
 	echo "${filename}" >> ./downloaded.txt
 	
 	### LINE通知
 	if [ "${LINE_TOKEN}" != "" ] ; then
-		curl -X POST -H "Authorization: Bearer ${LINE_TOKEN}" -F "message=録音完了:${filename//;/；}" https://notify-api.line.me/api/notify
+		curl -X POST -H "Authorization: Bearer ${LINE_TOKEN}" -F "message=録音完了:${filename//;/；}" -F "imageFile=@${WORK_DIR}/${target_title} ${title} ${delivery_date}放送${guest_title}.jpg" https://notify-api.line.me/api/notify
 	fi
+	rm ./*.jpg
 }
 
 function onsen_search(){
